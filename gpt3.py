aa26091e9b7d731cb0bd54626b73b342e337332c
Max Woolf <max@minimaxir.com>
2020-07-25 20:47:24 -0700

    simple interactivity

--- gpt3.py
+++ gpt3.py
@@ -1,5 +1,6 @@
 from gpt3_client import GPT3Client
 import fire
+from rich.prompt import Prompt, Confirm
 
 
 def gpt3_app(
@@ -10,9 +11,14 @@ def gpt3_app(
     stop="",
     bg: tuple = (31, 36, 40),
     accent: tuple = (0, 64, 0),
+    interactive=False,
 ):
 
     gpt3 = GPT3Client(image=image)
+
+    if interactive:
+        prompt = Prompt.ask("Enter a prompt for the GPT-3 API")
+
     gpt3.generate(
         prompt=prompt,
         temperature=temperature,
@@ -21,6 +27,22 @@ def gpt3_app(
         bg=bg,
         accent=accent,
     )
+
+    if interactive:
+        continue_gen = True
+        while continue_gen:
+            continue_gen = Confirm.ask(
+                "Do you wish to continue generating from the same prompt?"
+            )
+            if continue_gen:
+                gpt3.generate(
+                    prompt=prompt,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    stop=stop,
+                    bg=bg,
+                    accent=accent,
+                )
 
 
 if __name__ == "__main__":
